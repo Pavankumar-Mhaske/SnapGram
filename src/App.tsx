25657eec80c5604cfb66686ad71c54b38cbe9467
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-27 00:16:34 +0530

    chore: tailwind setup + basic stylings is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import "./globals.css";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      {" "}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
