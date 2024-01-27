12e977ce606035efff6a90f3afd18aaf2512b8a3
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-28 00:30:43 +0530

    chore: added new routes

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,14 @@ const App = () => {
         {/*🔏🔏 Private routes  🔏🔏*/}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
