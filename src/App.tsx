b691070bbbd73ff2772ebb18db7cb56070d909fc
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-28 01:07:21 +0530

    chore: changed imports statements

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ import RootLayout from "./_root/RootLayout";
 import { Home } from "./_root/pages";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import Explore from "./_root/pages/Explore";
-import Saved from "./_root/pages/Saved";
-import AllUsers from "./_root/pages/AllUsers";
-import CreatePost from "./_root/pages/CreatePost";
-import EditPost from "./_root/pages/EditPost";
-import PostDetails from "./_root/pages/PostDetails";
-import Profile from "./_root/pages/Profile";
-import UpdateProfile from "./_root/pages/UpdateProfile";
+import { Explore } from "./_root/pages";
+import { Saved } from "./_root/pages";
+import { AllUsers } from "./_root/pages";
+import { CreatePost } from "./_root/pages";
+import { EditPost } from "./_root/pages";
+import { PostDetails } from "./_root/pages";
+import { Profile } from "./_root/pages";
+import { UpdateProfile } from "./_root/pages";
 
 const App = () => {
   return (
