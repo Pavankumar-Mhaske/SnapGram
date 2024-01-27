8b3a28af8fe5dee4fee9e6ba9aa54d6b18d830e9
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-27 23:31:33 +0530

    chore: mutation for signout

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,7 +4,11 @@ import {
   useQueryClient,
   useInfiniteQuery,
 } from "@tanstack/react-query";
-import { createUserAccount, signInAccount } from "../appwrite/api";
+import {
+  createUserAccount,
+  signInAccount,
+  signOutAccount,
+} from "../appwrite/api";
 import { INewUser } from "@/types";
 
 export const useCreateUserAccount = () => {
@@ -17,5 +21,11 @@ export const useSignInAccount = () => {
   return useMutation({
     mutationFn: (user: { email: string; password: string }) =>
       signInAccount(user),
+  });
+};
+
+export const useSignOutAccount = () => {
+  return useMutation({
+    mutationFn: signOutAccount,
   });
 };
