0fdb66f0aa029ecac2f0af634c2b557d1f3c0551
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-27 22:59:50 +0530

    chore: wrapped entire app inside QueryProvider and AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import AuthProvider from "./context/AuthContext";
+import { QueryProvider } from "./lib/react-query/QueryProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <QueryProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryProvider>
   </BrowserRouter>
 );
