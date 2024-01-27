a6667ea748dc0a6d3e4b17cd5b59db776b9839da
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-27 18:45:05 +0530

    feat: created mutations and queries

---
+++ src/lib/react-query/queriesAndMutations.ts
@@ -0,0 +1,21 @@
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
+import { createUserAccount, signInAccount } from "../appwrite/api";
+import { INewUser } from "@/types";
+
+export const useCreateUserAccount = () => {
+  return useMutation({
+    mutationFn: (user: INewUser) => createUserAccount(user),
+  });
+};
+
+export const useSignInAccount = () => {
+  return useMutation({
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
+  });
+};
