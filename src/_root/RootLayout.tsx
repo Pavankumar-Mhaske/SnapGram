b4ca32ead378be63640512e9c1cfda4c89e38e94
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-28 00:23:40 +0530

    chore: changed the file name

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Topbar from "@/components/shared/Topbar";
-import Bottombar from "@/components/shared/bottombar";
+import Bottombar from "@/components/shared/Bottombar";
 import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
