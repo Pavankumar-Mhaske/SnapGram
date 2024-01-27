a3a563da6f03cab5ff724bd464340488deb94b4a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-27 23:00:27 +0530

    chore: signin validation

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -8,3 +8,10 @@ export const SignupValidation = z.object({
     .string()
     .min(8, { message: "Password must be at least 8 characters" }),
 });
+
+export const SigninValidation = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" }),
+});
