d5a2c6f945bea16f51da7b4723d4d0f7bf0357e6
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-27 17:08:07 +0530

    chore: after creating auth... save user to DB

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import { INewUser } from "@/types";
-import { account } from "./config";
-import { ID } from "appwrite";
+import { account, appwriteConfig, avatars, database } from "./config";
+import { Databases, ID } from "appwrite";
 
 export async function createUserAccount(user: INewUser) {
   try {
@@ -10,7 +10,40 @@ export async function createUserAccount(user: INewUser) {
       user.password,
       user.name
     );
-    return newAccount;
+
+    if (!newAccount) throw new Error("Account not created");
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.name,
+      imageUrl: avatarUrl,
+    });
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+export async function saveUserToDB(user: {
+  accountId: string;
+  name: string;
+  username?: string;
+  email: string;
+  imageUrl: URL;
+}) {
+  try {
+    const newUser = await database.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
+    return newUser;
   } catch (error) {
     console.log(error);
     return error;
