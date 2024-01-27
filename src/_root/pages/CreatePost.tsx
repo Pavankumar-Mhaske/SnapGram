b76efb69d3c55ae6471375fc321877b95131c6f4
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-28 01:14:16 +0530

    chore: started creating createPost page

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
-  )
-}
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="max-w-5xl flex-start gap-3 justify-start w-full">
+          <img
+            src="/assets/icons/add-post.svg"
+            alt="add"
+            width={36}
+            height={36}
+          />
+          <h2 className="h3-bold md:h2-bold text-left w-full ">Create Post</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CreatePost
+export default CreatePost;
