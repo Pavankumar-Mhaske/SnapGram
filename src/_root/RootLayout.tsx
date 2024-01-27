ef0cfece5fe6b1c0b48b2fb28898bb19987a7ed5
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-27 23:11:09 +0530

    chore: added all the components

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Topbar from "@/components/shared/Topbar";
+import Bottombar from "@/components/shared/bottombar";
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
-  )
-}
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+      <Bottombar />
+    </div>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
