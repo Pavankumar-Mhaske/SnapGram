885af824df571f89b54530a188ac8b5d1bbf3671
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-28 01:07:59 +0530

    chore: created the bottombar

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,7 +1,37 @@
+import { bottombarLinks } from "@/constants";
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Bottombar = () => {
-  return <div>Bottombar</div>;
+  const { pathname } = useLocation();
+
+  return (
+    <section className="bottom-bar">
+      {bottombarLinks.map((link) => {
+        const isActvie = pathname === link.route;
+        return (
+          <Link
+            to={link.route}
+            key={link.label}
+            className={` ${
+              isActvie && `bg-primary-500 rounded-[10px]`
+            } flex-center flex-col gap-1 p-2 transition`}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              width={16}
+              height={16}
+              className={`group-hover:invert-white ${
+                isActvie && "invert-white"
+              }`}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Bottombar;
