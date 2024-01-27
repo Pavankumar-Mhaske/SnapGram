29ad1818109ddd1100d841848f557585c441453a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-27 23:31:48 +0530

    feat: created Topbar

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import { useUserContext } from "@/context/AuthContext";
 
 const Topbar = () => {
-  return (
-    <div>Topbar</div>
-  )
-}
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  44;
 
-export default Topbar
+  const { user } = useUserContext();
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isSuccess) {
+      navigate(0);
+    }
+  }, [isSuccess]);
+
+  return (
+    <section className="topbar">
+      <div className="flex-between py-4 px-5">
+        <Link to="/" className="flex gap-3 items-center">
+          <img
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={130}
+            height={325}
+          />
+        </Link>
+        <div className="flex gap-4">
+          {/* LogOut button */}
+          <Button
+            variant="ghost"
+            className="shad-button_ghost"
+            onClick={() => signOut()}
+          >
+            <img src="/assets/icons/logout.svg" alt="logout" />
+          </Button>
+          {/* Profile Photo Link */}
+          <Link to={`/profile/${user.id}`} className="flex-center gap-3">
+            <img
+              src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+              alt="profile"
+              className="h-8 w-8 rounded-full"
+            />
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Topbar;
