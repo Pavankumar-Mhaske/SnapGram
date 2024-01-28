0febeed364616c89ae712d5c738fe279bdb8f23a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-28 16:29:16 +0530

    chore: showing post caption

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Loader from "@/components/shared/Loader";
+import PostCard from "@/components/shared/PostCard";
 import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
 import { Models } from "appwrite";
 import React from "react";
@@ -20,7 +21,7 @@ const Home = () => {
           ) : (
             <ul className="flex flex-col flex-1 gap-9 w-full">
               {posts?.documents.map((post: Models.Document) => (
-                <PostCard />
+                <PostCard key={post.caption} post={post} />
               ))}
             </ul>
           )}
