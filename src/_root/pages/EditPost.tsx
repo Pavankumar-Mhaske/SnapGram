04c5c82e76d792aeeaeefde57bc0993c2d2afb4f
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-28 17:16:40 +0530

    feat: created Editpost form

--- src/_root/pages/EditPost.tsx
+++ src/_root/pages/EditPost.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import PostForm from "@/components/forms/PostForm";
+import Loader from "@/components/shared/Loader";
+import { useGetPostById } from "@/lib/react-query/queriesAndMutations";
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const EditPost = () => {
-  return (
-    <div>EditPost</div>
-  )
-}
+  const { id } = useParams<{ id: string }>();
 
-export default EditPost
+  const { data: post, isPending } = useGetPostById(id || "");
+
+  if (isPending) return <Loader />;
+
+  return (
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="max-w-5xl flex-start gap-3 justify-start w-full">
+          <img
+            src="/assets/icons/add-post.svg"
+            alt="add"
+            width={36}
+            height={36}
+          />
+          <h2 className="h3-bold md:h2-bold text-left w-full ">Edit Post</h2>
+        </div>
+        <PostForm action="Update" post={post} />
+      </div>
+    </div>
+  );
+};
+
+export default EditPost;
