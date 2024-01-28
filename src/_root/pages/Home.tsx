0ae721dc4678676ab276774adb1556d44830d539
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-28 14:05:40 +0530

    chore: started of creating home page

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import Loader from "@/components/shared/Loader";
+import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
+import { Models } from "appwrite";
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const {
+    data: posts,
+    isPending: isPostLoading,
+    isError: isErrorPost,
+  } = useGetRecentPosts();
 
-export default Home
+  return (
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          {isPostLoading && !posts ? (
+            <Loader />
+          ) : (
+            <ul className="flex flex-col flex-1 gap-9 w-full">
+              {posts?.documents.map((post: Models.Document) => (
+                <PostCard />
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
