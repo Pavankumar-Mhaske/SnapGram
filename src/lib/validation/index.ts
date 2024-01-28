6a07d32270dcafe4294e9d65e15eaf034db8faa8
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-28 11:55:35 +0530

    chore: created PostValidation

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -15,3 +15,10 @@ export const SigninValidation = z.object({
     .string()
     .min(8, { message: "Password must be at least 8 characters" }),
 });
+
+export const PostValidation = z.object({
+  caption: z.string().min(2).max(2200),
+  file: z.custom<File[]>(),
+  location: z.string().min(2).max(100),
+  tags: z.string(),
+});
