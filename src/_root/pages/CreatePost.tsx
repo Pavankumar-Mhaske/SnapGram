3b9384b520eb2d24626fd85406491f6a62e72fa5
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-28 17:16:19 +0530

    chore: added action to the form

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -14,7 +14,7 @@ const CreatePost = () => {
           />
           <h2 className="h3-bold md:h2-bold text-left w-full ">Create Post</h2>
         </div>
-        <PostForm />
+        <PostForm action="Create" />
       </div>
     </div>
   );
