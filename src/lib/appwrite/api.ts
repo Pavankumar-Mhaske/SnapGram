5982eaecf31723f90ec5156a2a803fc79c0cc9ef
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-29 12:30:38 +0530

    chore: changed the limit for loading posts in advance

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -353,7 +353,7 @@ export async function deletePost(postId: string, imageId: string) {
 }
 
 export async function getInfinitePosts({ pageParam }: { pageParam: number }) {
-  const queries: any[] = [Query.orderDesc("$updatedAt"), Query.limit(10)];
+  const queries: any[] = [Query.orderDesc("$updatedAt"), Query.limit(4)];
   if (pageParam) queries.push(Query.cursorAfter(pageParam.toString()));
   try {
     const posts = await databases.listDocuments(
