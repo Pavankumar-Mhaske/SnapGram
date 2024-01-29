eb7ab8aeb18cda857909c68a0ec293a294c1cb9a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-29 15:19:47 +0530

    feat: created new component

---
+++ src/components/shared/UserCard.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Models } from "appwrite";
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
+type UserCardProps = {
+  user: Models.Document;
+};
+const UserCard = ({ user }: UserCardProps) => {
+  console.log("user", user);
+  return (
+    <Link to={`/profile/${user.$id}`} className="user-card">
+      <img
+        src="/assets/icons/profile-placeholder.svg"
+        alt="creator"
+        className="rounded-full w-14 h-14"
+      />
+      <div className="flex-center flex-col gap-1">
+        <p className="base-medium text-light-1 text-center line-clamp-1">
+          {user.name}
+        </p>
+        <p className="small-regular text-light-3 text-center line-clamp-1">
+          {user.username}
+        </p>
+      </div>
+      <Button type="button" size="sm" className="shad-button_primary px-5">
+        Follow
+      </Button>
+    </Link>
+  );
+};
+
+export default UserCard;
