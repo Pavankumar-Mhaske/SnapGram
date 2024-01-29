994de6750f16f537ef2bc82d56a79c695d9cd66a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-29 16:27:42 +0530

    feat: made this page dynamic...

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import Loader from "@/components/shared/Loader";
+import UserCard from "@/components/shared/UserCard";
+import { useToast } from "@/components/ui/use-toast";
+import { useGetUsers } from "@/lib/react-query/queriesAndMutations";
+import React from "react";
 
 const AllUsers = () => {
-  return (
-    <div>AllUsers</div>
-  )
-}
+  const { toast } = useToast();
+  const {
+    data: creators,
+    isPending: isUserLoading,
+    isError: isErrorCreators,
+  } = useGetUsers();
 
-export default AllUsers
+  if (isErrorCreators) {
+    toast({
+      title: "Something went wrong.",
+    });
+    return;
+  }
+
+  return (
+    <div className="common-container">
+      <div className="user-container">
+        <h2 className="h3-bold md:h2-bold text-left w-full">All Users</h2>
+
+        {isUserLoading && !creators ? (
+          <Loader />
+        ) : (
+          <ul className="user-grid">
+            {creators?.documents.map((creator) => (
+              <li key={creator?.$id} className="flex-1 min-w-[200px] w-full">
+                <UserCard user={creator} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AllUsers;
