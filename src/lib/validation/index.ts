3bb02aea13f86564e5a55ad9d535b5e30e988a1a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-30 04:23:25 +0530

    chore: ProfileValidation

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -16,6 +16,20 @@ export const SigninValidation = z.object({
     .min(8, { message: "Password must be at least 8 characters" }),
 });
 
+export const ProfileValidation = z.object({
+  file: z.custom<File[]>(),
+  name: z
+    .string()
+    .min(2, { message: "Name must be at least 2 characters" })
+    .max(50),
+  username: z
+    .string()
+    .min(2, { message: "Username must be at least 2 characters" })
+    .max(50),
+  email: z.string().email(),
+  bio: z.string().min(2).max(2200),
+});
+
 export const PostValidation = z.object({
   caption: z.string().min(2).max(2200),
   file: z.custom<File[]>(),
