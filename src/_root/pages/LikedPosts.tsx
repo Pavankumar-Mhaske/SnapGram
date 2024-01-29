e45dc99fe320df8822f1ec2b44eca73494ad6532
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-30 02:03:26 +0530

    feat: created liked posts page

--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import GridPostList from "@/components/shared/GridPostList";
+import Loader from "@/components/shared/Loader";
+import { useGetCurrentUser } from "@/lib/react-query/queriesAndMutations";
+import React from "react";
 
 const LikedPosts = () => {
-  return (
-    <div>LikedPosts</div>
-  )
-}
+  const { data: currentUser } = useGetCurrentUser();
 
-export default LikedPosts
+  if (!currentUser)
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+  return (
+    <>
+      {currentUser?.liked.length === 0 && (
+        <p className="text-light-4">No liked posts</p>
+      )}
+      <GridPostList posts={currentUser?.liked} showStats={false} />
+    </>
+  );
+};
+
+export default LikedPosts;
