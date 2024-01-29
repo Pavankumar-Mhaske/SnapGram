5cde3b30e906a94a75c3c2fcca47284e0d59aad4
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-29 16:58:14 +0530

    chore: defined getSavedPosts key

--- src/lib/react-query/queryKeys.ts
+++ src/lib/react-query/queryKeys.ts
@@ -14,6 +14,7 @@ export enum QUERY_KEYS {
   GET_POST_BY_ID = "getPostById",
   GET_USER_POSTS = "getUserPosts",
   GET_FILE_PREVIEW = "getFilePreview",
+  GET_SAVED_POSTS = "getSavedPosts",
 
   //  SEARCH KEYS
   SEARCH_POSTS = "getSearchPosts",
