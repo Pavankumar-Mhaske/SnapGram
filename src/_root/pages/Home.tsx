0815c382b3091242b7beac420d4eb89a6f73b9df
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-29 15:41:46 +0530

    chore: instead of blocking entirePage... just block that section which is under bad request

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -21,54 +21,71 @@ const Home = () => {
     isError: isErrorCreator,
   } = useGetUsers(10);
 
-  if (isErrorPost || isErrorCreator) {
-    return (
-      <div className="flex flex-1">
-        <div className="home-container">
-          <p className="body-medium text-light-1">Something bad happened</p>
-        </div>
-        <div className="home-creators">
-          {" "}
-          <p className="body-medium text-light-1">Something bad happened</p>
-        </div>
-      </div>
-    );
-  }
+  // if (isErrorPost || isErrorCreator) {
+  //   return (
+  //     <div className="flex flex-1">
+  //       <div className="home-container">
+  //         <p className="body-medium text-light-1">Something bad happened</p>
+  //       </div>
+  //       <div className="home-creators">
+  //         <p className="body-medium text-light-1">Something bad happened</p>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
   console.log("creators", creators);
   return (
     <div className="flex flex-1">
-      <div className="home-container">
-        <div className="home-posts">
+      {!isErrorPost ? (
+        <div className="home-container">
+          <div className="home-posts">
+            <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+            {isPostLoading && !posts ? (
+              <Loader />
+            ) : (
+              <ul className="flex flex-col flex-1 gap-9 w-full">
+                {posts?.documents.map((post: Models.Document) => (
+                  <li key={post.$id} className="flex justify-center w-full">
+                    <PostCard key={post.caption} post={post} />
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className="home-container">
           <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
-          {isPostLoading && !posts ? (
+
+          <p className="body-medium text-light-1">
+            Oops, Something bad happened
+          </p>
+        </div>
+      )}
+      {!isErrorCreator ? (
+        <div className="home-creators">
+          <h3 className="h3-bold text-light-1">Top Creators</h3>
+          {isUserLoading && !creators ? (
             <Loader />
           ) : (
-            <ul className="flex flex-col flex-1 gap-9 w-full">
-              {posts?.documents.map((post: Models.Document) => (
-                <li key={post.$id} className="flex justify-center w-full">
-                  <PostCard key={post.caption} post={post} />
+            <ul className="grid 2xl:grid-cols-2 gap-6">
+              {creators?.documents.map((creator) => (
+                <li key={creator?.$id}>
+                  <UserCard user={creator} />
                 </li>
               ))}
             </ul>
           )}
         </div>
-      </div>
-
-      <div className="home-creators">
-        <h3 className="h3-bold text-light-1">Top Creators</h3>
-        {isUserLoading && !creators ? (
-          <Loader />
-        ) : (
-          <ul className="grid 2xl:grid-cols-2 gap-6">
-            {creators?.documents.map((creator) => (
-              <li key={creator?.$id}>
-                <UserCard user={creator} />
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+      ) : (
+        <div className="home-creators">
+          <h3 className="h3-bold text-light-1">Top Creators</h3>
+          <p className="body-medium text-light-1">
+            Oops, Something bad happened
+          </p>
+        </div>
+      )}
     </div>
   );
 };
