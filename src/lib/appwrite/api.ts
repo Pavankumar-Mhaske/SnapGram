c7ee5949769bf8fd179a8ba46b965b0e30262c31
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-30 19:01:36 +0530

    chore: username can also be updated now

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -465,6 +465,7 @@ export async function updateUser(user: IUpdateUser) {
       user.userId,
       {
         name: user.name,
+        username: user.username,
         bio: user.bio,
         imageUrl: image.imageUrl,
         imageId: image.imageId,
