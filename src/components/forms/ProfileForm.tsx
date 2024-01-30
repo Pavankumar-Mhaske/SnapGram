b3d84ea37a4ac52715993651ce6015ec307a336a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-30 18:26:07 +0530

    chore: created saparate component for edit profile page... so that reloading the page problem is tackeled

---
+++ src/components/forms/ProfileForm.tsx
@@ -0,0 +1,191 @@
+import React from "react";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate, useParams } from "react-router-dom";
+
+import ProfileUploader from "@/components/shared/ProfileUploader";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useToast } from "@/components/ui/use-toast";
+import { ProfileValidation } from "@/lib/validation";
+import {
+  useGetUserById,
+  useUpdateUser,
+} from "@/lib/react-query/queriesAndMutations";
+import Loader from "@/components/shared/Loader";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Models } from "appwrite";
+
+type UserFormProps = {
+  user?: Models.Document;
+};
+
+const ProfileForm = ({ user }: UserFormProps) => {
+  const { toast } = useToast();
+  const navigate = useNavigate();
+  const { id } = useParams<{ id: string }>();
+  console.log("id : ", id);
+  console.log("user : ", user);
+
+  const form = useForm<z.infer<typeof ProfileValidation>>({
+    resolver: zodResolver(ProfileValidation),
+    defaultValues: {
+      file: [],
+      name: user ? user?.name : "dummy name",
+      username: user ? user?.username : "dummy username",
+      email: user ? user?.email : "dummy email",
+      bio: user ? user?.bio : "",
+    },
+  });
+
+  console.log("forms : ", form);
+
+  // Queries
+  const { data: currentUser } = useGetUserById(id || "");
+  console.log("currentUser : ", currentUser);
+  const { mutateAsync: updateUser, isPending: isLoadingUpdate } =
+    useUpdateUser();
+
+  if (!currentUser)
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+
+  // Handler
+  const handleUpdate = async (value: z.infer<typeof ProfileValidation>) => {
+    const updatedUser = await updateUser({
+      userId: currentUser?.$id || "",
+      name: value.name,
+      bio: value.bio,
+      file: value.file,
+      imageUrl: currentUser?.imageUrl,
+      imageId: currentUser?.imageId,
+    });
+
+    if (!updatedUser) {
+      toast({
+        title: `Update user failed, Please try again.`,
+      });
+    }
+
+    return navigate(`/profile/${id}`);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleUpdate)}
+        className="flex flex-col gap-7 w-full mt-4 max-w-5xl"
+      >
+        <FormField
+          control={form.control}
+          name="file"
+          render={({ field }) => (
+            <FormItem className="flex">
+              <FormControl>
+                <ProfileUploader
+                  fieldChange={field.onChange}
+                  mediaUrl={currentUser?.imageUrl}
+                />
+              </FormControl>
+              <FormMessage className="shad-form_message" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Name</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-input" {...field} />
+              </FormControl>
+              <FormMessage className="shad-form_message" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Username</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-input" {...field} disabled />
+              </FormControl>
+              <FormMessage className="shad-form_message" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Email</FormLabel>
+              <FormControl>
+                <Input
+                  type="email"
+                  className="shad-input"
+                  {...field}
+                  disabled
+                />
+              </FormControl>
+              <FormMessage className="shad-form_message" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Bio</FormLabel>
+              <FormControl>
+                <Textarea
+                  className="shad-textarea custom-scrollbar"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="shad-form_message" />
+            </FormItem>
+          )}
+        />
+
+        <div className="flex gap-4 items-center justify-end">
+          <Button
+            type="button"
+            className="shad-button_dark_4"
+            onClick={() => navigate(-1)}
+          >
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            className="shad-button_primary whitespace-nowrap"
+          >
+            Update Profile
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default ProfileForm;
