7c6968274ca33178e5aceec61d700f5cccbd9a03
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-30 19:53:38 +0530

    chore: created getUserPosts

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -261,6 +261,23 @@ export async function deleteSavedPost(savedRecordId: string) {
   }
 }
 
+// ============================== GET USER'S POST
+export async function getUserPosts(userId?: string) {
+  if (!userId) return;
+
+  try {
+    const post = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+      [Query.equal("creator", userId), Query.orderDesc("$createdAt")]
+    );
+    if (!post) throw Error("No post found");
+    return post;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export async function getPostById(postId: string) {
   try {
     const post = await databases.getDocument(
