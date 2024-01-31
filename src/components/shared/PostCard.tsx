d2a89e18b01ffc25032dbab21c005fc9d3c9181b
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-01-31 15:15:18 +0530

    chore: changed the height to make fully circular...

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -24,7 +24,7 @@ const PostCard = ({ post }: PostCardProps) => {
                 "/assets/icons/profile-placeholder.svg"
               }
               alt="creator"
-              className="rounded-full w-12 lg:h-12"
+              className="rounded-full w-12 h-12 lg:h-12"
             />
           </Link>
           <div className="flex flex-col">
