cabfb49dba0b1de0d273e8ea8a15cc93e62b6314
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-01 15:14:57 +0530

    chore: function is capable of fetching infinite users

--- src/components/shared/UserCard.tsx
+++ src/components/shared/UserCard.tsx
@@ -3,29 +3,48 @@ import { Models } from "appwrite";
 import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
 type UserCardProps = {
-  user: Models.Document;
+  users: Models.Document[] | "";
 };
-const UserCard = ({ user }: UserCardProps) => {
-  console.log("user", user);
+
+// {/* <ul className="user-grid">
+//   {creators?.pages.map((creator, index) => (
+//     <li key={creator?.$id} className="flex-1 min-w-[200px] w-full">
+//       <UserCard user={creator} />
+//     </li>
+//   ))}
+// </ul>; */}
+
+const UserCard = ({ users }: UserCardProps) => {
+  console.log("user", users);
   return (
-    <Link to={`/profile/${user.$id}`} className="user-card">
-      <img
-        src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
-        alt="creator"
-        className="rounded-full w-14 h-14"
-      />
-      <div className="flex-center flex-col gap-1">
-        <p className="base-medium text-light-1 text-center line-clamp-1">
-          {user.name}
-        </p>
-        <p className="small-regular text-light-3 text-center line-clamp-1">
-          {user.username}
-        </p>
-      </div>
-      <Button type="button" size="sm" className="shad-button_primary px-5">
-        Follow
-      </Button>
-    </Link>
+    <ul className="user-grid">
+      {users.map((user) => (
+        <li key={user?.$id} className="flex-1 min-w-[200px] w-full">
+          <Link to={`/profile/${user.$id}`} className="user-card">
+            <img
+              src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+              alt="creator"
+              className="rounded-full w-14 h-14"
+            />
+            <div className="flex-center flex-col gap-1">
+              <p className="base-medium text-light-1 text-center line-clamp-1">
+                {user.name}
+              </p>
+              <p className="small-regular text-light-3 text-center line-clamp-1">
+                {user.username}
+              </p>
+            </div>
+            <Button
+              type="button"
+              size="sm"
+              className="shad-button_primary px-5"
+            >
+              Follow
+            </Button>
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
