98b925d5b97858ef0064ffb94d9f976dedf60452
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-01 14:39:55 +0530

    chore: just a bit extra safety

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PostStats from "./PostStats";
 type GridPostListProps = {
-  posts: Models.Document[];
+  posts: Models.Document[] | "";
   showUser?: boolean;
   showStats?: boolean;
 };
