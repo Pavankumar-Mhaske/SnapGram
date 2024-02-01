616f1df364414ace7de534927f4ac8faed69c6e6
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-01 14:40:48 +0530

    chore: completety reform the component to handle the infinite posts

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -6,68 +6,89 @@ import { Link } from "react-router-dom";
 import PostStats from "./PostStats";
 
 type PostCardProps = {
-  post: Models.Document;
+  posts: Models.Document[] | "";
 };
 
-const PostCard = ({ post }: PostCardProps) => {
+//   <ul className="flex flex-col flex-1 gap-9 w-full">
+//   {posts?.documents.map((post: Models.Document) => (
+//     <li key={post.$id} className="flex justify-center w-full">
+//       <PostCard key={post.caption} post={post} />
+//     </li>
+//   ))}
+// </ul>
+
+const PostCard = ({ posts }: PostCardProps) => {
   const { user } = useUserContext();
   console.log("Current user", user);
-  if (!post.creator) return;
+
+  // if (!post.creator) return;
+
   return (
-    <div className="post-card">
-      <div className="flex-between">
-        <div className="flex items-center gap-3">
-          <Link to={`/profile/${post.creator.$id}`}>
-            <img
-              src={
-                post?.creator?.imageUrl ||
-                "/assets/icons/profile-placeholder.svg"
-              }
-              alt="creator"
-              className="rounded-full w-12 h-12 lg:h-12"
-            />
-          </Link>
-          <div className="flex flex-col">
-            <p className="base-medium lg:body-bold text-light-1">
-              {post.creator.name}
-            </p>
-            <div className="flex-center gap-2 text-light-3">
-              <p className="subtle-semibold lg:small-regular">
-                {multiFormatDateString(post.$createdAt)}
-              </p>
-              -
-              <p className="subtle-semibold lg:small-regular">
-                {post.location}
-              </p>
+    <ul className="flex flex-col flex-1 gap-9 w-full">
+      {posts.map((post) => (
+        <li key={post.$id} className="flex justify-center w-full">
+          <div className="post-card">
+            <div className="flex-between">
+              <div className="flex items-center gap-3">
+                <Link to={`/profile/${post.creator.$id}`}>
+                  <img
+                    src={
+                      post?.creator?.imageUrl ||
+                      "/assets/icons/profile-placeholder.svg"
+                    }
+                    alt="creator"
+                    className="rounded-full w-12 h-12 lg:h-12"
+                  />
+                </Link>
+                <div className="flex flex-col">
+                  <p className="base-medium lg:body-bold text-light-1">
+                    {post.creator.name}
+                  </p>
+                  <div className="flex-center gap-2 text-light-3">
+                    <p className="subtle-semibold lg:small-regular">
+                      {multiFormatDateString(post.$createdAt)}
+                    </p>
+                    -
+                    <p className="subtle-semibold lg:small-regular">
+                      {post.location}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <Link
+                to={`/update-post/${post.$id}`}
+                className={`${user.id !== post.creator.$id && "hidden"}`}
+              >
+                <img
+                  src="/assets/icons/edit.svg"
+                  alt="edit"
+                  width={20}
+                  height={20}
+                />
+              </Link>
             </div>
+            <Link to={`/posts/${post.$id}`}>
+              <div className="small-medium lg:base-medium py-5 ">
+                <p>{post.caption}</p>
+                <ul className="flex gap-1 mt-2">
+                  {post.tags?.map((tag: string) => (
+                    <li key={tag} className="text-light-3 ">
+                      #{tag}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <img
+                src={post.imageUrl || "/assets/icons/profile-placeholder.svg"}
+                alt="post image"
+                className="post-card_img "
+              />
+            </Link>
+            <PostStats post={post} userId={user.id} />
           </div>
-        </div>
-        <Link
-          to={`/update-post/${post.$id}`}
-          className={`${user.id !== post.creator.$id && "hidden"}`}
-        >
-          <img src="/assets/icons/edit.svg" alt="edit" width={20} height={20} />
-        </Link>
-      </div>
-      <Link to={`/posts/${post.$id}`}>
-        <div className="small-medium lg:base-medium py-5 ">
-          <p>{post.caption}</p>
-          <ul className="flex gap-1 mt-2">
-            {post.tags?.map((tag: string) => (
-              <li key={tag} className="text-light-3 ">
-                #{tag}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <img
-          src={post.imageUrl || "/assets/icons/profile-placeholder.svg"}
-          alt="post image"
-          className="post-card_img "
-        />
-      </Link>
-      <PostStats post={post} userId={user.id} />
-    </div>
+        </li>
+      ))}
+    </ul>
   );
 };
 
