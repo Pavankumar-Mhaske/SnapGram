a5cc040baa1d024ac47264cc2b77697ce38404a9
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-04-17 02:33:20 +0530

    chore: removing unncecessay stuff

--- src/lib/react-query/QueryProvider.tsx
+++ src/lib/react-query/QueryProvider.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 
 const queryClient = new QueryClient();
 export const QueryProvider = ({ children }: { children: ReactNode }) => {
