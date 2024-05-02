d9fb033f4f1cc63994dc794385c5eeedb1775cdd
P1kumarMhaske <mhaskepavankumar@gmail.com>
2024-05-02 18:31:26 +0530

    chore: done with the queryProvider
    Co-Authored-By: P1kumarMhaske <mhaskepavankumar@gmail.com>

--- src/lib/react-query/QueryProvider.tsx
+++ src/lib/react-query/QueryProvider.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 
+
 const queryClient = new QueryClient();
 export const QueryProvider = ({ children }: { children: ReactNode }) => {
   return (
